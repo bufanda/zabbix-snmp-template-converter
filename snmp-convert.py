7aabf813cd0e75dbe526d1e575b9338af529f73c
bufanda <thorsten@bufanda.de>
2025-02-07 08:20:14 +0100

    snmp-converter: do not add the get to already converted items

--- snmp-convert.py
+++ snmp-convert.py
@@ -20,13 +20,15 @@ for item in input["zabbix_export"]:
 for template in input["zabbix_export"]["templates"]:
     for item in template["items"]:
         if item["type"] == "SNMP_AGENT":
-            item.update({"snmp_oid": "get[" + item["snmp_oid"] + "]"})
+            if "get[" not in item["snmp_oid"]:
+                item.update({"snmp_oid": "get[" + item["snmp_oid"] + "]"})
 
     for discovery in template["discovery_rules"]:
         item_prototypes = discovery["item_prototypes"]
         for prototype in item_prototypes:
-            if prototype["type"] == "SNMP_AGENT":
-                prototype.update({"snmp_oid": "get[" + prototype["snmp_oid"] +"]"})
+            if prototype["type"] == "SNMP_AGENT": 
+                if "get[" not in prototype["snmp_oid"]:
+                    prototype.update({"snmp_oid": "get[" + prototype["snmp_oid"] +"]"})
 
 print(input)
 with open("converted_template.yml", "w") as output:
