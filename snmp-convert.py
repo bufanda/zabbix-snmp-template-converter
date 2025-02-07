c3615626db6ea4e2ac1ebdab395d5a08781ff279
bufanda <thorsten@bufanda.de>
2025-02-07 13:13:58 +0100

    implement discovery rule management

--- snmp-convert.py
+++ snmp-convert.py
@@ -26,10 +26,39 @@ for template in input["zabbix_export"]["templates"]:
     for discovery in template["discovery_rules"]:
         item_prototypes = discovery["item_prototypes"]
         for prototype in item_prototypes:
-            if prototype["type"] == "SNMP_AGENT": 
+            if prototype["type"] == "SNMP_AGENT":
                 if "get[" not in prototype["snmp_oid"]:
                     prototype.update({"snmp_oid": "get[" + prototype["snmp_oid"] +"]"})
 
-print(input)
+    for rule in template["discovery_rules"]:
+        macros = []
+        oids = []
+        preprocessing = []
+        if rule["type"] == "SNMP_AGENT":
+            if "discovery[" in rule["snmp_oid"]:
+                oids_macros = rule["snmp_oid"][len("discovery["):-1]
+                oids_macros = str.split(oids_macros, ",")
+
+                for index,value in enumerate(oids_macros):
+                    if index%2 == 0:
+                        macros.append(value)
+                    else:
+                        oids.append(value)
+                rule["snmp_oid"] = "walk[" + str.join(",", oids) + "]"
+                pp = {
+                    "type": "SNMP_WALK_TO_JSON",
+                    "parameters": []
+                }
+                for index,value in enumerate(macros):
+                    pp["parameters"].append(value)
+                    pp["parameters"].append(oids[index])
+                    pp["parameters"].append('0')
+                preprocessing.append(pp)
+            if "preprocessing" in rule:
+                rule["preprocessing"] = rule["preprocessing"] + preprocessing
+            else:
+                rule.update({"preprocessing": preprocessing})
+
+
 with open("converted_template.yml", "w") as output:
     yaml.safe_dump(input,output,)
