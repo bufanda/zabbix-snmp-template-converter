db760c4849bc7b7d276bcc2d4e6c7f83c8f67f55
bufanda <30717829+bufanda@users.noreply.github.com>
2025-08-26 10:58:19 +0200

    Update snmp-convert.py

--- snmp-convert.py
+++ snmp-convert.py
@@ -4,12 +4,14 @@
 #
 #####
 
-import ruamel.yaml as YAML
+import yaml # as YAML
+import uuid
 
-yaml = YAML.YAML()
+# yaml = YAML.YAML()
 
 with open("template.yml", "r") as inputf:
-    input = yaml.load(inputf)
+    input = yaml.safe_load(inputf)
+
 
 for item in input["zabbix_export"]:
     if item == "version":
@@ -20,47 +22,73 @@ for item in input["zabbix_export"]:
             exit(-1)
 
 for template in input["zabbix_export"]["templates"]:
+
+    old_name = template["name"]
+    template["name"] = f"{template["name"]} asynchron"
+    template["template"] = f"{template["template"]} asynchron"
+    template["uuid"] = uuid.uuid4().hex
+
     for item in template["items"]:
         if item["type"] == "SNMP_AGENT":
             if "get[" not in item["snmp_oid"]:
                 item.update({"snmp_oid": "get[" + item["snmp_oid"] + "]"})
+            if "triggers" in item:
+                for triggers in item["triggers"]:
+                    if old_name in triggers["expression"]:
+                        triggers["expression"] =  triggers["expression"].replace(old_name, template["template"])
+                    triggers["uuid"] = uuid.uuid4().hex
+        item["uuid"] = uuid.uuid4().hex
 
-    for discovery in template["discovery_rules"]:
-        item_prototypes = discovery["item_prototypes"]
-        for prototype in item_prototypes:
-            if prototype["type"] == "SNMP_AGENT":
-                if "get[" not in prototype["snmp_oid"]:
-                    prototype.update({"snmp_oid": "get[" + prototype["snmp_oid"] +"]"})
+    if "triggers" in template:
+        for triggers in template["triggers"]:
+            if old_name in triggers["expression"]:
+                triggers["expression"] =  triggers["expression"].replace(old_name, template["template"])
+            triggers["uuid"] = uuid.uuid4().hex
 
-    for rule in template["discovery_rules"]:
-        macros = []
-        oids = []
-        preprocessing = []
-        if rule["type"] == "SNMP_AGENT":
-            if "discovery[" in rule["snmp_oid"]:
-                oids_macros = rule["snmp_oid"][len("discovery["):-1]
-                oids_macros = str.split(oids_macros, ",")
+    if "discovery_rules" in template:
+        for discovery in template["discovery_rules"]:
+            item_prototypes = discovery["item_prototypes"]
+            for prototype in item_prototypes:
+                if prototype["type"] == "SNMP_AGENT":
+                    if "get[" not in prototype["snmp_oid"]:
+                        prototype.update({"snmp_oid": "get[" + prototype["snmp_oid"] +"]"})
+                prototype["uuid"] = uuid.uuid4().hex
+            discovery["uuid"] = uuid.uuid4().hex
+        
+        for rule in template["discovery_rules"]:
+            macros = []
+            oids = []
+            preprocessing = []
+            if rule["type"] == "SNMP_AGENT":
+                if "discovery[" in rule["snmp_oid"]:
+                    oids_macros = rule["snmp_oid"][len("discovery["):-1]
+                    oids_macros = str.split(oids_macros, ",")
 
-                for index,value in enumerate(oids_macros):
-                    if index%2 == 0:
-                        macros.append(value)
-                    else:
-                        oids.append(value)
-                rule["snmp_oid"] = "walk[" + str.join(",", oids) + "]"
-                pp = {
-                    "type": "SNMP_WALK_TO_JSON",
-                    "parameters": []
-                }
-                for index,value in enumerate(macros):
-                    pp["parameters"].append(value)
-                    pp["parameters"].append(oids[index])
-                    pp["parameters"].append('0')
-                preprocessing.append(pp)
-            if "preprocessing" in rule:
-                rule["preprocessing"] = rule["preprocessing"] + preprocessing
-            else:
-                rule.update({"preprocessing": preprocessing})
+                    for index,value in enumerate(oids_macros):
+                        if index%2 == 0:
+                            macros.append(value)
+                        else:
+                            oids.append(value)
+                    rule["snmp_oid"] = "walk[" + str.join(",", oids) + "]"
+                    pp = {
+                        "type": "SNMP_WALK_TO_JSON",
+                        "parameters": []
+                    }
+                    for index,value in enumerate(macros):
+                        pp["parameters"].append(value)
+                        pp["parameters"].append(oids[index])
+                        pp["parameters"].append('0')
+                    preprocessing.append(pp)
+                if "preprocessing" in rule:
+                    rule["preprocessing"] = rule["preprocessing"] + preprocessing
+                else:
+                    rule.update({"preprocessing": preprocessing})
+            if "triggers" in rule:
+                for triggers in rule["triggers"]:
+                    if old_name in triggers["expression"]:
+                        triggers["expression"] =  triggers["expression"].replace(old_name, template["template"])
 
 
 with open("converted_template.yml", "w") as output:
     yaml.dump(input,output)
+    
